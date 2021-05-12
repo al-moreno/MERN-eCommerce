7a0bedf91b94b34e8bdd399264a145ee95669d88
Ana Laura Moreno <almoreno@themorenofamily.com>
2021-05-12 08:20:36 -0500

    worked on tweeking css and index. Also added basic mapping for products using id, name, image and pricing

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "Black Casual Pants",
+            category: "Men's Pants",
+            image: "/images/Men-Pant-Black.jpg",
+            price: 50.00,
+            brand: "Unknown",
+            rating: 4,
+            numReviews: 6,
+            description: "Great comfort casual pants",
+
+        },
+        {
+            _id: "1",
+            name: "Brown Casual Pants",
+            category: "Men's Pants",
+            image: "/images/Men-Pant-Brown.jpg",
+            price: 67.00,
+            brand: "Unknown",
+            rating: 5,
+            numReviews: 12,
+            description: "High quality, great comfort pants",
+
+        },
+        {
+            _id: "3",
+            name: "Grey Casual Pants",
+            category: "Men's Pants",
+            image: "/images/Men-Pant-Grey.jpg",
+            price: 24.00,
+            brand: "Unknown",
+            rating: 2,
+            numReviews: 3,
+            description: "Casual pants",
+
+        },
+        {
+            _id: "4",
+            name: "Black Casual Pants",
+            category: "Men's Pants",
+            image: "/images/Men-Pant-Black.jpg",
+            price: 75.00,
+            brand: "Unknown",
+            rating: 4,
+            numReviews: 10,
+            description: "High quality, great comfort casual pants",
+
+        },
+        {
+            _id: "5",
+            name: "Black Casual Pants",
+            category: "Men's Pants",
+            image: "/images/Men-Pant-Brown.jpg",
+            price: 50.00,
+            brand: "Unknown",
+            rating: 4,
+            numReviews: 6,
+            description: "High quality, great comfort casual pants",
+
+        },
+        {
+            _id: "6",
+            name: "Black Casual Pants",
+            category: "Men's Pants",
+            image: "/images/Men-Pant-Grey.jpg",
+            price: 50.00,
+            brand: "Unknown",
+            rating: 4,
+            numReviews: 6,
+            description: "High quality, great comfort casual pants",
+
+        },
+    ],
+};
+
+export default data;
