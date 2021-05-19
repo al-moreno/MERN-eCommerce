a24b2ff84a515f3dc94f123396e4c94af05d1539
Ana Laura Moreno <almoreno@themorenofamily.com>
2021-05-19 10:39:10 -0500

    changes to cart to display photos of product

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -13,12 +13,12 @@ function Cart() {
             {cart && cart.length > 0 ? cart.map((item, idx) => {
                 return <div key={idx} className='card'>
                     <table className='single-row'>
-                        <tr><td><img className="small" src={item.image} alt={item.name} /></td>
-                            <td className='name-cell'><span className='item-name'>{item.name}</span><br />
-                                <span className='item-desc'>{item.description}</span></td>
-                            <td className='item-price'><b>Price: </b> ${item.price.toFixed(2)}</td>
-                            <td className='item-quantity'><b>Quantity: </b>{item.quantity}</td>
-                            <td className='item-price'><b>Total: ${item.quantity * item.price}</b></td>
+                        <tr>
+                            <td className='name-cell'><span className='item-name'>{item.name}</span><br /> <br />
+                            <img className="small" src={item.image} alt={item.name} /></td>
+                            <td className='item-price'><b>Price: </b> <br />${item.price.toFixed(2)}</td>
+                            <td className='item-quantity'><b>Quantity: </b><br />{item.quantity}</td>
+                            <td className='item-price'><b>Total:<br /> ${item.quantity * item.price}</b></td>
                         </tr>
                     </table>
                 </div>
@@ -26,7 +26,7 @@ function Cart() {
             }) : 'No Items in the cart'}
                     <ul>
                         <li>
-                            <button className="block">Proceed to Checkout</button>
+                            <button  href="Checkout.js" className="block">Proceed to Checkout</button>
                         </li>
                     </ul>
         </main>
